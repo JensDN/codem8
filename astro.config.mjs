7071bee206cb2b0198bdb46897eb9dd43df7a3c9
JensDN <jens.deneef@gmail.com>
2024-08-12 14:14:11 +0200

    Second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [starlight(
+    {
+      title: 'My delightful docs site',
+    }
+  ),mdx(), sitemap(), ]
 });
