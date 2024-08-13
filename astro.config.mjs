c2b712bd5a1d9c5d215d7209465cfa50a0ed99ef
JensDN <jens.deneef@gmail.com>
2024-08-13 11:04:11 +0200

    feat(app): Added tailwind to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import starlight from "@astrojs/starlight";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [starlight(
-    {
-      title: 'My delightful docs site',
-    }
-  ),mdx(), sitemap(), ]
+  integrations: [starlight({
+    title: 'My delightful docs site',
+    customCss: [
+      // Path to your Tailwind base styles:
+      './src/styles/tailwind.css',
+    ],
+  }), mdx(), sitemap(), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false,
+  }),]
 });
