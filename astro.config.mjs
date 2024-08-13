8a771fda65953fdd759c6302145ca28fd01cee47
JensDN <jens.deneef@gmail.com>
2024-08-13 11:08:55 +0200

    Added prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,26 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [starlight({
-    title: 'My delightful docs site',
-    customCss: [
-      // Path to your Tailwind base styles:
-      './src/styles/tailwind.css',
-    ],
-  }), mdx(), sitemap(), tailwind({
-    // Disable the default base styles:
-    applyBaseStyles: false,
-  }),]
+  site: "https://example.com",
+  integrations: [
+    starlight({
+      title: "My delightful docs site",
+      customCss: [
+        // Path to your Tailwind base styles:
+        "./src/styles/tailwind.css",
+      ],
+    }),
+    mdx(),
+    sitemap(),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+  ],
 });
