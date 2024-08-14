f88a49f83f35139a05f9c918807f42c15c2a9533
JensDN <jens.deneef@gmail.com>
2024-08-14 10:52:41 +0200

    feat(app): :sparkles: Add tailwind style and theming

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightBlog from 'starlight-blog'
+import starlightBlog from 'starlight-blog';
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'CODEM8',
-			plugins: [starlightBlog({
-				/* sidebar: [
-					{
-						label: 'Guides',
-						items: [
-							// Each item here is one entry in the navigation menu.
-							{ label: 'Example Guide', slug: 'guides/example' },
-						],
-					},
-					{
-						label: 'Reference',
-						autogenerate: { directory: 'reference' },
-					},
-				], */
-
-			})],
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			customCss: [
-				// Fontsource files for to regular and semi-bold font weights.
-				'@fontsource-variable/fira-code',
-				'./src/styles/custom.css'
-			  ],
-		}),
-	],
+	integrations: [starlight({
+		title: 'CODEM8',
+		plugins: [starlightBlog({
+			/* sidebar: [
+				  {
+					  label: 'Guides',
+					  items: [
+						  // Each item here is one entry in the navigation menu.
+						  { label: 'Example Guide', slug: 'guides/example' },
+					  ],
+				  },
+				  {
+					  label: 'Reference',
+					  autogenerate: { directory: 'reference' },
+				  },
+			], */
+		})],
+		social: {
+			github: 'https://github.com/withastro/starlight'
+		},
+		customCss: [
+			// Fontsource files for to regular and semi-bold font weights.
+			'@fontsource-variable/fira-code',
+			// Path to Custom base styles
+			'./src/styles/custom.css',
+			// Path to Tailwind base styles:
+			'./src/styles/tailwind.css'
+		]
+	}), tailwind({
+		// Disable the default base styles:
+		applyBaseStyles: false,
+	  }),]
 });
