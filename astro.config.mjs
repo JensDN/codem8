ff509de7400a179978359d6e6b32a2341d88c7a8
JensDN <jens.deneef@gmail.com>
2024-08-23 14:12:56 +0200

    build(app): :heavy_plus_sign: Add astro react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,31 +4,32 @@ import starlightBlog from 'starlight-blog';
 import tailwind from "@astrojs/tailwind";
 import netlify from '@astrojs/netlify';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jensdn.github.io',
-	output: 'server',
-	adapter: netlify(),
-	integrations: [starlight({
-		title: 'CODEM8',
-		plugins: [starlightBlog()],
-		components: {
-			// Override the default `Header` component.
-			Header: './src/components/Header.astro',
-		},
-		social: {
-			github: 'https://github.com/JensDN'
-		},
-		customCss: [
-			// Fontsource files for to regular and semi-bold font weights.
-			'@fontsource-variable/fira-code',
-			// Path to Custom base styles
-			'./src/styles/custom.css',
-			// Path to Tailwind base styles:
-			'./src/styles/tailwind.css'
-		]
-	}), tailwind({
-		// Disable the default base styles:
-		applyBaseStyles: false,
-	}),]
+  site: 'https://jensdn.github.io',
+  output: 'server',
+  adapter: netlify(),
+  integrations: [starlight({
+    title: 'CODEM8',
+    plugins: [starlightBlog()],
+    components: {
+      // Override the default `Header` component.
+      Header: './src/components/Header.astro'
+    },
+    social: {
+      github: 'https://github.com/JensDN'
+    },
+    customCss: [
+    // Fontsource files for to regular and semi-bold font weights.
+    '@fontsource-variable/fira-code',
+    // Path to Custom base styles
+    './src/styles/custom.css',
+    // Path to Tailwind base styles:
+    './src/styles/tailwind.css']
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false
+  }), react()]
 });
