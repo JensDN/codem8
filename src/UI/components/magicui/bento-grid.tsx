67942dd5285955bb86595aaa8ec3c37a76b08d86
JensDN <jens.deneef@gmail.com>
2024-09-04 18:22:23 +0200

    refactor(projecten): :fire: Rework on bentogrid

---
+++ src/UI/components/magicui/bento-grid.tsx
@@ -0,0 +1,73 @@
+
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
+import type { ReactNode } from "react";
+import { Button } from "../ui/button";
+
+const BentoGrid = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid w-full auto-rows-[22rem] grid-cols-3 gap-4",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const BentoCard = ({
+  name,
+  className,
+  background,
+  description,
+  href,
+  cta,
+}: {
+  name: string;
+  className: string;
+  background: ReactNode;
+  description: string;
+  href: string;
+  cta: string;
+}) => (
+  <div
+    key={name}
+    className={cn(
+      "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
+      className,
+    )}
+  >
+    <div>{background}</div>
+    <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
+      <h3 className="text-xl font-semibold text-foreground">
+        {name} test
+      </h3>
+      <p className="max-w-lg text-white">{description}</p>
+    </div>
+
+    <div
+      className={cn(
+        "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100",
+      )}
+    >
+      <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
+        <a href={href}>
+          {cta}
+          <ArrowRightIcon className="ml-2 h-4 w-4" />
+        </a>
+      </Button>
+    </div>
+    <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
+  </div>
+);
+
+export { BentoCard, BentoGrid };
---
+++ src/components/react/projects/index.tsx
@@ -0,0 +1,24 @@
+import { BentoCard, BentoGrid } from "@/UI/components/magicui/bento-grid";
+import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import nonGramRef from '@/assets/projects-cards/non-gram.jpeg';
+
+const features = [
+  {
+    name: "NONO VS GRAM",
+    description: "A Nonogram versus game",
+    href: "/",
+    cta: "Learn more",
+    background: <img className="absolute opacity-50" src={nonGramRef.src} />,
+    className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3 text-black",
+  },
+];
+
+export function ProjectsIndex() {
+  return (
+    <BentoGrid className="lg:grid-rows-3">
+      {features.map((feature) => (
+        <BentoCard  key={feature.name} {...feature} />
+      ))}
+    </BentoGrid>
+  );
+}
