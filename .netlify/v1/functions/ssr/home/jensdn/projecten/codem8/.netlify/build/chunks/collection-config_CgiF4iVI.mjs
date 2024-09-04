c9637acd235e2e5807e690353d3b1ed9e3324313
JensDN <jens.deneef@gmail.com>
2024-09-04 18:47:35 +0200

    build(app): create new build v2

--- .netlify/v1/functions/ssr/home/jensdn/projecten/codem8/.netlify/build/chunks/collection-config_CgiF4iVI.mjs
+++
@@ -1,7 +0,0 @@
-let userCollections;
-			try {
-				userCollections = (await import('./config_Bn7Ft6iq.mjs')).collections;
-			} catch {}
-			const collections = userCollections;
-
-export { collections };
